55cf734ec089dc6fb13f6f633579581de425a4cc
flerken <solely.flerken@proton.me>
2024-01-24 19:57:23 +0100

    :coffin: Remove unused imports

--- src/main/java/com/flerken/springbase/api/controller/SampleControllerApi.java
+++ src/main/java/com/flerken/springbase/api/controller/SampleControllerApi.java
@@ -1,15 +1,13 @@
 package com.flerken.springbase.api.controller;
 
-import com.flerken.springbase.api.controller.base.BaseCRUDControllerApi;
 import com.flerken.springbase.api.dto.ResponseDto;
 import com.flerken.springbase.api.dto.SampleDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-public interface SampleControllerApi extends BaseCRUDControllerApi<SampleDto> {
+public interface SampleControllerApi {
     @GetMapping("/special")
     ResponseEntity<ResponseDto<List<SampleDto>>> retrieveBySpecificConditions();
 }
